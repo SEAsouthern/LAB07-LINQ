efd33c1e60c2641eede91c509fc6ee23eb83f4a1
Matthew Johnson <matthew.james.johnson@hotmail.com>
2020-03-30 09:33:59 -0700

    Add project files.

---
+++ LAB07-LINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace LAB07_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
